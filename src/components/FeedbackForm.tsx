7a7757febeb325b4177f53158c0b51884b8a0cb3
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-13 22:05:03 -0700

    stub out feedback form

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,3 +1,20 @@
 export default function FeedbackForm() {
-  return <div>FeedbackForm</div>;
+  return (
+    <form className="form">
+      <textarea
+        id="feedback-textarea"
+        placeholder="" //  add empty placeholder to enable label visibility causing label to have 'present' attribute
+        spellCheck="false"
+      ></textarea>
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
+      <div>
+        <p className="u-italic">150</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
 }
