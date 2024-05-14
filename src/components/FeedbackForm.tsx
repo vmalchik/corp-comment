0d88b4d63964bff7f83d553e62744f3421880ac7
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-13 22:44:31 -0700

    made feedback form a controlled component

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,16 +1,40 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
+// Controlled input textarea
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+
+  const count = text.length;
+  const remaining = MAX_CHARACTERS - count;
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    let newText = event.target.value;
+
+    // // guard clause
+    // if (newText.length > MAX_CHARACTERS) {
+    //   return;
+    // }
+
+    setText(newText);
+  };
+
   return (
     <form className="form">
       <textarea
         id="feedback-textarea"
         placeholder="" //  add empty placeholder to enable label visibility causing label to have 'present' attribute
         spellCheck="false"
-      ></textarea>
+        onChange={handleChange}
+        maxLength={MAX_CHARACTERS}
+      >
+        {text}
+      </textarea>
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{remaining}</p>
         <button>
           <span>Submit</span>
         </button>
