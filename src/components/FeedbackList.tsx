580977af88be842a82518be48f27785fe8778bda
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-13 22:29:42 -0700

    stub feedback item

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,28 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
 export default function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>593</span>
+        </button>
+
+        <div>
+          <p>B</p>
+        </div>
+
+        <div>
+          <p>CompanyName</p>
+          {/* Generate following in VSCode using lorem15 */}
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
+            quae porro molestiae harum earum saepe?
+          </p>
+        </div>
+
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 }
